4e974db719933f82c6fc73ca051f375ed4d586ed
Navid Rigi Ebrahimi <narigi-e@c2r11s1.42wolfsburg.de>
2024-02-23 17:31:51 +0100

    files added

---
+++ ex01/Fixed.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+
+class Fixed
+{
+	public:
+		Fixed();
+		Fixed(const Fixed &other);
+		Fixed &operator = (const Fixed &other);
+		~Fixed();
+		int		getRawBits( void ) const;
+		void	setRawBits( int const raw );
+	private:
+		int					value_;
+		static const int	fractional_ = 8;
+};
